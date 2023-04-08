c62f2f3ba22e937ce1b909a893afffa01587ad56
Tharicq1409 <sheiktharicq14@gmail.com>
2023-04-08 19:22:15 +0530

    i changed some changes in  html and models

--- modelproj/myapp/views.py
+++ modelproj/myapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from myapp.models import Person
+
 # Create your views here.
+
 def user_details(request):
     person =Person.objects.all()
     context={'person':person}
